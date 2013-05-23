2ce4e4983721c6b0426d09383e60f16179cab148
TaeHwanLee <thlee@manaolabs.com>
2013-05-23 12:18:33 +0900

    Step 0 - Bootstrapping

--- project/Build.scala
+++ project/Build.scala
@@ -7,15 +7,13 @@ object ApplicationBuild extends Build {
   val appName         = "phonecat"
   val appVersion      = "1.0-SNAPSHOT"
 
-  val appDependencies = Seq(
-    // Add your project dependencies here,
-    jdbc,
-    anorm
-  )
+  val appDependencies = Seq()
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+    lessEntryPoints <<= (sourceDirectory in Compile)(base => (
+      (base / "assets" / "stylesheets"/ "bootstrap" / "bootstrap.less" )
+      ))
   )
 
 }
